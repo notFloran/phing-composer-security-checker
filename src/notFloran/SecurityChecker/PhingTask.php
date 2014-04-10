7b25e0395a196b1b0fffe2729d693b235166b53c
Floran Brutel <f.brutel@hexanet.fr>
2014-04-10 13:38:47 +0200

    Is now possible to set the composer.lock file SHow a message when no errors otherwise an exception

--- src/notFloran/SecurityChecker/PhingTask.php
+++ src/notFloran/SecurityChecker/PhingTask.php
@@ -2,14 +2,31 @@
 
 namespace notFloran\SecurityChecker;
 
+use SensioLabs\Security\SecurityChecker;
+
 use Task;
 use Project;
 use BuildException;
 
 class PhingTask extends Task
 {
+    protected $file = "composer.lock";
+
     public function main($transport = null)
     {
-        throw new BuildException('Security Checker exception');
+        $checker = new SecurityChecker();
+        $alerts = $checker->check($this->file, 'json');
+
+        if(empty($alerts)) {
+            $this->log("Great!");
+            $this->log("The checker did not detected known* vulnerabilities in your project dependencies.");
+            return;
+        }
+
+        throw new BuildException('Security errors');
+    }
+
+    public function setFile($file) {
+        $this->file = $file;
     }
 }
