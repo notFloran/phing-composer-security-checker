9b1f3fc6f277ef3d46ef7b89e56706a6da4a9cc4
Floran Brutel <f.brutel@hexanet.fr>
2014-04-11 13:31:19 +0200

    Handle format of the result Is now possible to store the result in a property Better display of the result

--- src/notFloran/SecurityChecker/PhingTask.php
+++ src/notFloran/SecurityChecker/PhingTask.php
@@ -22,35 +22,102 @@ class PhingTask extends Task
      *
      * @var boolean
      */
-    protected $checkreturn = true;
+    protected $haltonerror = true;
+
+    /**
+     * The return format : text or json
+     *
+     * @var string
+     */
+    protected $format = "text";
+
+    /**
+     * Property name to set with output value
+     *
+     * @var string
+     */
+    protected $outputProperty;
 
     public function main()
     {
-        $checker = new SecurityChecker();
-        $alerts = json_decode($checker->check($this->file, 'json'), true);
+        if(!in_array($this->format, ['text', 'json'])) {
+            throw new BuildException(sprintf('Unsupported format "%s"',$this->format));
+        }
+
+        if(!file_exists($this->file)) {
+            throw new BuildException('File not found');
+        }
+
+        try {
+            $checker = new SecurityChecker();
+            $alerts = $checker->check($this->file, $this->format);
+        } catch (\Exception $e) {
+            throw new BuildException('Exception with SecurityChecker : ' . $e->getMessage());
+        }
 
         if(empty($alerts)) {
             $this->log("Great! The checker did not detected known* vulnerabilities in your project dependencies.");
             return;
         }
 
+        print($alerts);
+
+        if ($this->outputProperty) {
+            $this->project->setProperty($this->outputProperty, $alerts);
+        }
+
         $this->logError("Caution ! The checker detected package(s) that have known* vulnerabilities in your project. We recommend you to check the related security advisories and upgrade these dependencies.");
     }
 
+    /**
+     * Path to the composer.lock file
+     *
+     * @param string $file
+     */
     public function setFile($file) {
         $this->file = $file;
     }
 
-    public function setCheckreturn($checkreturn) {
-        $this->checkreturn = $checkreturn;
+    /**
+     * Indicate if we thrown an error when vulnerabilities are detected
+     *
+     * @param boolean $value
+     */
+    public function setHaltonerror($value)
+    {
+        $this->haltonerror = $value;
+    }
+
+    /**
+     * Set the format (json or text)
+     *
+     * @param string $format
+     */
+    public function setFormat($format) {
+        $this->format = $format;
+    }
+
+    /**
+     * Sets the Property name to set with output value.
+     *
+     * @param string $outputProperty the output property
+     *
+     * @return self
+     */
+    public function setOutputProperty($outputProperty)
+    {
+        $this->outputProperty = $outputProperty;
+
+        return $this;
     }
 
     protected function logError($message, $location = NULL)
     {
-        if ($this->checkreturn) {
+        if ($this->haltonerror) {
             throw new BuildException($message, $location);
         } else {
             $this->log($message, Project::MSG_ERR);
         }
     }
+
 }
